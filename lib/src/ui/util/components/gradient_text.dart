871d2621376f08374b74a9f6a7729cb5c429cd73
redDwarf03 <sylvain@archethic.net>
2024-02-14 13:37:58 +0100

    🚧 Add first components

---
+++ lib/src/ui/util/components/gradient_text.dart
@@ -0,0 +1,38 @@
+import 'package:archethic_dapp_framework_flutter/src/ui/util/components/scale_size.dart';
+import 'package:flutter/material.dart';
+
+class GradientText extends StatelessWidget {
+  const GradientText(
+    this.text, {
+    super.key,
+    required this.gradient,
+    this.style,
+    this.selectable = true,
+  });
+
+  final String text;
+  final TextStyle? style;
+  final Gradient gradient;
+  final bool selectable;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: selectable
+          ? SelectableText(
+              text,
+              style: style,
+              textScaleFactor: ScaleSize.textScaleFactor(context),
+            )
+          : Text(
+              text,
+              style: style,
+              textScaleFactor: ScaleSize.textScaleFactor(context),
+            ),
+    );
+  }
+}
---
+++ lib/src/ui/util/components/popup_template.dart
@@ -0,0 +1,103 @@
+import 'dart:ui';
+
+import 'package:archethic_dapp_framework_flutter/src/ui/themes/app_theme_base.dart';
+import 'package:archethic_dapp_framework_flutter/src/ui/util/components/popup_close_button.dart';
+import 'package:flutter/material.dart';
+
+class PopupTemplate extends StatelessWidget {
+  const PopupTemplate({
+    super.key,
+    required this.popupContent,
+    required this.popupTitle,
+    required this.popupHeight,
+    this.warningCloseLabel = '',
+    this.warningCloseFunction,
+  });
+
+  final Widget popupContent;
+  final String popupTitle;
+  final double popupHeight;
+  final String warningCloseLabel;
+  final Function? warningCloseFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            backgroundColor: Colors.transparent.withAlpha(120),
+            body: AlertDialog(
+              insetPadding: EdgeInsets.zero,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              content: Stack(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: Container(
+                        margin:
+                            const EdgeInsets.only(top: 30, right: 15, left: 8),
+                        padding: const EdgeInsets.all(20),
+                        height: popupHeight,
+                        width: AppThemeBase.sizeBoxComponentWidth,
+                        decoration: BoxDecoration(
+                          color: AppThemeBase.sheetBackground,
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
+                            color: AppThemeBase.sheetBorder,
+                          ),
+                        ),
+                        child: popupContent,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 15,
+                            ),
+                            child: SelectionArea(
+                              child: Text(
+                                popupTitle,
+                                style: Theme.of(context).textTheme.titleMedium,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              decoration: BoxDecoration(
+                                gradient: AppThemeBase.gradient,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: PopupCloseButton(
+                      warningCloseLabel: warningCloseLabel,
+                      warningCloseFunction: warningCloseFunction,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
