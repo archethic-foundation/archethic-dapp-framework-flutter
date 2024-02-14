816937ad1de25a5885a0d78e3ecc0b53eeecf5f1
redDwarf03 <sylvain@archethic.net>
2024-02-14 18:59:43 +0100

    🚧 New components

---
+++ lib/src/application/oracle/provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$archethicOracleUCONotifierHash() =>
+    r'fde11cb7f4b6081d6382632fa10c99bef1454aa2';
+
+/// See also [_ArchethicOracleUCONotifier].
+@ProviderFor(_ArchethicOracleUCONotifier)
+final _archethicOracleUCONotifierProvider =
+    NotifierProvider<_ArchethicOracleUCONotifier, ArchethicOracleUCO>.internal(
+  _ArchethicOracleUCONotifier.new,
+  name: r'_archethicOracleUCONotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$archethicOracleUCONotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ArchethicOracleUCONotifier = Notifier<ArchethicOracleUCO>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
