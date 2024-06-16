b10734b983933e508f4ba1a427d63c3738ba63d0
redDwarf03 <sylvain@archethic.net>
2024-06-16 22:39:44 +0200

    🚚 Move coin price management to an infrastructure repository

---
+++ lib/src/domain/repositories/tokens/coin_price.repository.dart
@@ -0,0 +1,9 @@
+/// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'package:archethic_dapp_framework_flutter/src/domain/models/crypto_price.dart';
+
+abstract class CoinPriceRepositoryInterface {
+  Future<CryptoPrice?> fetchPrices();
+
+  double getPriceFromUcid(int ucid, CryptoPrice coinPrice);
+}
