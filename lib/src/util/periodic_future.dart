89455f1dea6b3397407e1ed8848be4bd9d0cf1d7
redDwarf03 <sylvain@archethic.net>
2024-07-08 11:19:19 +0200

    🐛 Fix periodic future management

--- lib/src/util/periodic_future.dart
+++ lib/src/util/periodic_future.dart
@@ -1,18 +1,34 @@
 import 'dart:async';
 
 class PeriodicFuture {
-  /// Executes [action] until verification succeeds.
-  /// It waits [sleepDuration] between each run.
   static Future<T> periodic<T>(
     FutureOr<T> Function() action, {
     required Duration sleepDuration,
     required bool Function(T value) until,
+    Completer<void>? cancelCompleter,
   }) async {
     late T result;
-    await Future.doWhile(() async {
-      result = await action();
-      return !until(result);
-    });
+    try {
+      await Future.doWhile(() async {
+        result = await action();
+        if (until(result)) {
+          return false;
+        }
+        await Future.any(
+          [
+            Future.delayed(sleepDuration),
+            if (cancelCompleter != null) cancelCompleter.future,
+          ],
+        );
+        return cancelCompleter == null || !cancelCompleter.isCompleted;
+      });
+    } catch (e) {
+      if (cancelCompleter != null && cancelCompleter.isCompleted) {
+        // If the cancelCompleter was completed, it means we should stop the loop.
+        return result;
+      }
+      rethrow;
+    }
     return result;
   }
 }
