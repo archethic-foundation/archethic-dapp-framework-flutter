d973d37f057ac1b6c9acd5df1574a233910860c8
Chralu <contact@charly-caulet.net>
2024-08-05 14:17:29 +0200

    refactor: Use constants for UCIDs.

---
+++ lib/src/domain/models/ucid.dart
@@ -0,0 +1,8 @@
+abstract class UCID {
+  static const bitcoin = 1;
+  static const matic = 3890;
+  static const ethereum = 1027;
+  static const bnb = 1839;
+  static const usdc = 3408;
+  static const eure = 20920;
+}
